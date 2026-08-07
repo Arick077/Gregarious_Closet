946818f7caa0a6fcd6981d4ca70a5a8c6555a2c1
mison68 <misondeb68@gmail.com>
2026-08-07 08:01:22 +0600

    initializing how it work section (setup)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import HeroLp from "./components/HeroLp";
+import HowItwork from "./components/HowItWork";
 function App() {
   return (
        <div>
           <Navbar/>
           <HeroLp/>
+          <HowItwork/>
        </div>
   );
 }
---
+++ src/components/HowItWork.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function HowItwork() {
+    return(
+        <div>
+            initializing how it work section (setup)
+        </div>
+    )
+}
